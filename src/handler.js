a41c0860922b1538e163c6a9a6d4af95b08e8fe1
rijal2 <merentas5benua27@gmail.com>
2021-05-17 21:18:10 +0700

    Berkas pembuatan aplikasi sederhana menggunakan node js dan hapi.js

--- src/handler.js
+++ src/handler.js
@@ -2,121 +2,143 @@ const { nanoid } = require('nanoid');
 const books = require('./books');
 
 const addNoteHandler = (request, h) => {
-    const { title, tags, body } = request.payload;
-   
-    const id = nanoid(16);
-    const createdAt = new Date().toISOString();
-    const updatedAt = createdAt;
-   
-    const newBook = {
-      title, tags, body, id, createdAt, updatedAt,
-    };
-   
-    books.push(newBook);
-   
-    const isSuccess = books.filter((buku) => buku.id === id).length > 0;
-   
-    if (isSuccess) {
-      const response = h.response({
-        status: 'success',
-        message: 'Catatan berhasil ditambahkan',
-        data: {
-          bukuId: id,
-        },
-      });
-      response.code(201);
-      return response;
-    }
+  const {
+    name,
+    year,
+    author,
+    summary,
+    publisher,
+    pageCount,
+    readPage,
+    reading,
+  } = request.payload;
+
+
+  const id = nanoid(16);
+  const insertedAt = new Date().toISOString();
+  const updatedAt = insertedAt;
+
+  const newBook = {
+    id,
+    name,
+    year,
+    author,
+    summary,
+    publisher,
+    pageCount,
+    readPage,
+    finished,
+    reading,
+    insertedAt,
+    updatedAt,
+  };
+  console.log(newBook);
+  books.push(newBook);
+
+  const isSuccess = books.filter((buku) => buku.id === id).length > 0;
+
+  if (isSuccess) {
     const response = h.response({
-      status: 'fail',
-      message: 'Catatan gagal ditambahkan',
+      status: 'success',
+      message: 'Catatan berhasil ditambahkan',
+      data: {
+        bukuId: id,
+      },
     });
-    response.code(500);
+    response.code(201);
     return response;
+  }
+  const response = h.response({
+    status: 'fail',
+    message: 'Catatan gagal ditambahkan',
+  });
+  response.code(500);
+  response.header('Access-Control-Allow-Origin', '*');
+  return response;
 };
 
 const getAllNotesHandler = () => ({
-    status: 'success',
-    data: {
-      buku,
-    },
+  status: 'success',
+  data: {
+    buku,
+  },
 });
-   
 
-  
+
+
 const getNoteByIdHandler = (request, h) => {
-    const { id } = request.params;
-   
-    const buku = books.filter((buku) => buku.id === id)[0];
-   
-   if (buku !== undefined) {
-      return {
-        status: 'success',
-        data: {
-          buku,
-        },
-      };
-    }
-   
-    const response = h.response({
-      status: 'fail',
-      message: 'Catatan tidak ditemukan',
-    });
-    response.code(404);
-    return response;
+  const { id } = request.params;
+
+  const buku = books.filter((buku) => buku.id === id)[0];
+
+  if (buku !== undefined) {
+    return {
+      status: 'success',
+      data: {
+        buku,
+      },
+    };
+  }
+
+  const response = h.response({
+    status: 'fail',
+    message: 'Catatan tidak ditemukan',
+  });
+  response.code(404);
+  return response;
 };
 
 const editNoteByIdHandler = (request, h) => {
-    const { id } = request.params;
- 
-    const { title, tags, body } = request.payload;
-    const updatedAt = new Date().toISOString();
-    const index = books.findIndex((buku) => buku.id === id);
+  const { id } = request.params;
 
-    if (index !== -1) {
-        books[index] = {
-          ...books[index],
-          title,
-          tags,
-          body,
-          updatedAt,
-        };
-        const response = h.response({
-          status: 'success',
-          message: 'Catatan berhasil diperbarui',
-        });
-        response.code(200);
-        return response;
-      }
-      const response = h.response({
-        status: 'fail',
-        message: 'Gagal memperbarui catatan. Id tidak ditemukan',
-      });
-      response.code(404);
-      return response;
+  const { title, tags, body } = request.payload;
+  const updatedAt = new Date().toISOString();
+  const index = books.findIndex((buku) => buku.id === id);
+
+  if (index !== -1) {
+    books[index] = {
+      ...books[index],
+      title,
+      tags,
+      body,
+      updatedAt,
+    };
+    const response = h.response({
+      status: 'success',
+      message: 'Catatan berhasil diperbarui',
+    });
+    response.code(200);
+    return response;
+  }
+  const response = h.response({
+    status: 'fail',
+    message: 'Gagal memperbarui catatan. Id tidak ditemukan',
+  });
+  response.code(404);
+  return response;
 };
 
 const deleteNoteByIdHandler = (request, h) => {
-    const { id } = request.params;
-   
-    const index = books.findIndex((buku) => buku.id === id);
-   
-    if (index !== -1) {
-      books.splice(index, 1);
-      const response = h.response({
-        status: 'success',
-        message: 'Catatan berhasil dihapus',
-      });
-      response.code(200);
-      return response;
-    }
-   
-   const response = h.response({
-      status: 'fail',
-      message: 'Catatan gagal dihapus. Id tidak ditemukan',
+  const { id } = request.params;
+
+  const index = books.findIndex((buku) => buku.id === id);
+
+  if (index !== -1) {
+    books.splice(index, 1);
+    const response = h.response({
+      status: 'success',
+      message: 'Catatan berhasil dihapus',
     });
-    response.code(404);
+    response.code(200);
     return response;
+  }
+
+  const response = h.response({
+    status: 'fail',
+    message: 'Catatan gagal dihapus. Id tidak ditemukan',
+  });
+  response.code(404);
+  return response;
 };
-   
+
 module.exports = { addNoteHandler, getAllNotesHandler, getNoteByIdHandler, editNoteByIdHandler, deleteNoteByIdHandler };
--- src/routes.js
+++ src/routes.js
@@ -1,41 +1,42 @@
 const { addNoteHandler, getAllNotesHandler, getNoteByIdHandler, editNoteByIdHandler, deleteNoteByIdHandler, } = require('./handler');
+console.log(addNoteHandler);
 const routes = [
     {
       method: 'POST',
       path: '/books',
-      handler: () => {},
+      handler: addNoteHandler,
     },
     {
       method: 'GET',
       path: '/books',
       handler: getAllNotesHandler,
     },
-    {
-      method: 'GET',
-      path: '/books/{id}',
-      handler: () => {},
-    },
+    // {
+    //   method: 'GET',
+    //   path: '/books/{id}',
+    //   handler: () => {},
+    // },
 
     {
       method: 'GET',
       path: '/books/{id}',
       handler: getNoteByIdHandler,
     },
-    {
-      method: 'PUT',
-      path: '/books/{id}',
-      handler: () => {},
-    },
+    // {
+    //   method: 'PUT',
+    //   path: '/books/{id}',
+    //   handler: () => {},
+    // },
     {
       method: 'PUT',
       path: '/books/{id}',
       handler: editNoteByIdHandler,
     },
-    {
-      method: 'DELETE',
-      path: '/books/{id}',
-      handler: () => {},
-    },
+    // {
+    //   method: 'DELETE',
+    //   path: '/books/{id}',
+    //   handler: () => {},
+    // },
     {
       method: 'DELETE',
       path: '/books/{id}',
